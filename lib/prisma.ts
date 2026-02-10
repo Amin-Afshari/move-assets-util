ca13a64c17143de2fc46591bba874f3f37294cfc
Amin Afshari <aminafshari1998@gmail.com>
2026-02-10 14:03:12 +0330

    refactor(prisma): switch from accelerate extension to pg adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "../generated/prisma";
-// import { PrismaPg } from '@prisma/adapter-pg';
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
+// import { withAccelerate } from "@prisma/extension-accelerate";
 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL,
-// });
-// const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate());
+// const prisma = new PrismaClient({
+//   accelerateUrl: process.env.DATABASE_URL,
+// }).$extends(withAccelerate());
 
 export { prisma };
